01b82aa89260ba86e766b913cb39c284de284df9
Vova <man12-spb@instrument-fit.ru>
2020-07-06 16:24:34 +0300

    write arithmetic operation

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
-let num1, num2, result;
-
-num1 = document.getElementById('num1').value;
-num2 = document.getElementById('num2').value;
-
-
 function plus() {
+    let num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
     result = num1 + num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+function minus() {
+    let num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result = num1 - num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+function mult() {
+    let num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result = num1 * num2;
+    document.getElementById('out').innerHTML = result;
+}
+
+function div() {
+    let num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+
+    num2 = document.getElementById('n2').value;
+    num2 = parseInt(num2);
+
+    result = num1 / num2;
+    document.getElementById('out').innerHTML = result;
 }
