4aa0fa56aaa8f97b025ade3ed00e4c660c598310
NWpiter <northboss@gmail.com>
2020-07-07 22:56:16 +0300

    Write small game

---
+++ y1.js
@@ -0,0 +1,23 @@
+let prNum, tempOut;
+prNum = Math.floor((Math.random() * 10) + 1);
+// tempOut = document.getElementById("temp-out");
+// tempOut.innerHTML = prNum;
+console.log(prNum);
+
+function f1() {
+  let num, out;
+
+
+  num = document.getElementById("mynum").value;
+  out = document.getElementById("out");
+
+  if (num == prNum) {
+    out.innerHTML = "Угадал, умник!";
+  }
+  else if (num > prNum) {
+    out.innerHTML = "Число должно быть меньше";
+  }
+  else {
+    out.innerHTML = "Число должно быть больше!";
+  }
+}
